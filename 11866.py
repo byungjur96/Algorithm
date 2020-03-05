4effdeb08400dd0f658d8caacd9279bade42132f
Byungju Chae <cbyungjub@naver.com>
2020-03-05 19:02:32 +0900

    (2020.03.05) 요세푸스 문제 0

---
+++ 11866.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+
+people = [(i+1) for i in range(n)]
+result = []
+
+pos = -1
+
+
+for i in range(n):
+    count = 0
+    while count != k:
+        pos += 1
+        if people[pos % n] is not None:
+            count += 1
+    result.append(str(people[pos % n]))
+    people[(pos % n)] = None
+print("<" + ", ".join(result) + ">")
+
