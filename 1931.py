e1c8206730c28a04954b9d9b5a464402d98ed7ba
Byungju Chae <cbyungjub@naver.com>
2020-03-06 10:24:13 +0900

    (2020.03.06) 회의실배정

--- 1931.py
+++ 1931.py
@@ -8,9 +8,9 @@ end_time = 0  # 회의 막 끝난 시
 for _ in range(num):
     meetings.append(list(map(int, input().split())))
 
-meetings.sort(key=lambda m: m[0])
-meetings.sort(key=lambda m: m[1])
-print(meetings)
+# meetings.sort(key=lambda m: m[0])
+# meetings.sort(key=lambda m: m[1])
+meetings.sort(key=lambda m: [m[1], m[0]])
 for [start, end] in meetings:
     if start >= end_time:
         end_time = end
