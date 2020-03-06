bd348f8724778c183c14360756e2255a690021d7
Byungju Chae <cbyungjub@naver.com>
2020-03-06 10:20:24 +0900

    (2020.03.06) 회의실배정

---
+++ 1931.py
@@ -0,0 +1,19 @@
+import sys
+
+num = int(input())
+meetings = []
+total = 0
+end_time = 0  # 회의 막 끝난 시
+
+for _ in range(num):
+    meetings.append(list(map(int, input().split())))
+
+meetings.sort(key=lambda m: m[0])
+meetings.sort(key=lambda m: m[1])
+print(meetings)
+for [start, end] in meetings:
+    if start >= end_time:
+        end_time = end
+        total += 1
+
+print(total)
