cff8307c1bbfce9aff878ad08d999485d600d5ed
Byungju Chae <cbyungjub@naver.com>
2022-06-21 21:45:12 +0900

    (2020.06.21) 순열 사이클

---
+++ 10451.py
@@ -0,0 +1,29 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    lst = list(map(int, sys.stdin.readline().split()))
+    root = [i+1 for i in range(n)]
+    net = dict()
+    for i in range(n):
+        net[i+1] = [i+1]
+    e = []
+    for idx, l in enumerate(lst):
+        if root[idx] > root[l-1]:
+            temp = root[idx]
+            for n in net[root[idx]]:
+                net[root[l-1]].append(n)
+                root[n-1] = root[l-1]
+            del net[temp]
+            root[idx] = root[l-1]
+        elif root[idx] < root[l-1]:
+            temp = root[l-1]
+            for n in net[root[l-1]]:
+                net[root[idx]].append(n)
+                root[n-1] = root[idx]
+            del net[temp]
+            root[l-1] = root[idx]
+    print(len(net))
+        
