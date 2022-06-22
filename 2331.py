b8c088bcb040f22a10b059bd375c003227f30b0c
Byungju Chae <cbyungjub@naver.com>
2022-06-22 15:02:40 +0900

    (2020.06.22) 반복수열

--- 2331.py
+++ 2331.py
@@ -2,11 +2,12 @@ def cal(n, p):
     result = 0
     lst = list(map(int,list(str(n))))
     for l in lst:
-        result += (l**p)
+        result += power[l]
     return result
 
 a, p = map(int, input().split())
 exist = []
+power = [i**p for i in range(10)]
 val = a
 
 while val not in exist:
