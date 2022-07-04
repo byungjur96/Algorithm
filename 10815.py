b08df984efdec25eff7f0fd38988ad3e518e82c9
Byungju Chae <cbyungjub@naver.com>
2022-07-04 21:57:27 +0900

    (2020.07.04) 숫자 카드

--- 10815.py
+++ 10815.py
@@ -5,15 +5,12 @@ def check_existance(lst, k):
     low = -1
     while low + 1 < high:
         mid = (high + low) // 2
-        print(low, mid, high)
         if lst[mid] > k:
             high = mid
         elif lst[mid] < k:
             low = mid
         else:
-            print(1)
             return "1"
-    print(0)
     return "0"
 
 m = int(sys.stdin.readline())
