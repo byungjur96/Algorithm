024baa5ee30477f72e21a2ef72481669eb94ba0e
Byungju Chae <cbyungjub@naver.com>
2022-07-04 20:43:38 +0900

    (2020.07.04) 랜선 자르기

--- 1654.py
+++ 1654.py
@@ -1,30 +1,24 @@
 import sys
 
-k, n = map(int, input().split())
-lans = []
-result = 0
-low = 0
-high = 0
-mid = (low + high) // 2
+def check(arr, l):
+    result = 0
+    for a in arr:
+        result += (a // l)
+    return result
 
+k, n = map(int, sys.stdin.readline().split())
+lan = []
 for _ in range(k):
-    val = int(sys.stdin.readline().rstrip())
-    lans.append(val)
-    if val > high:
-        high = val
+    lan.append(int(sys.stdin.readline()))
 
-num = 1  # 조각의 개수
-while low <= high:
-    num = 0
-    mid = max(1, (low + high) // 2)
-    for lan in lans:
-        num += lan // mid
-    # 조각이 덜 나온 경우 -> 길이를 짧게
-    if num < n:
-        high = mid - 1
-    # 조각이 더 나온 경우 -> 길이를 길게
-    else:
-        result = max(mid, result)
-        low = mid + 1
-
-print(result)
+low = 1
+high = max(lan) + 1
+mid = high // 2
+while low + 1 < high:
+    if check(lan, mid) >= n:
+        low = mid
+    elif check(lan, mid) < n:
+        high = mid
+    mid = (high + low) // 2
+    
+print(low)
