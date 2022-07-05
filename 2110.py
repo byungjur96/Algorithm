1753085efd61fdef09bcd708b07d1a0df889996c
Byungju Chae <cbyungjub@naver.com>
2022-07-05 14:46:59 +0900

    (2020.07.05) 공유기 설치

--- 2110.py
+++ 2110.py
@@ -1,33 +1,26 @@
 import sys
 
-n, c = map(int, sys.stdin.readline().rstrip().split())
-xi = []
-for _ in range(n):
-    xi.append(int(sys.stdin.readline().rstrip()))
+n, c = map(int, sys.stdin.readline().split())
+house = []
+for i in range(n):
+    house.append(int(sys.stdin.readline()))
+house.sort()
+low = 0
+high = (house[-1] // (c - 1)) + 1
 
-
-def router_count(dis):
-    total = 1
-    pos = xi[0]
-    for i in range(1, n):
-        if xi[i] - pos >= dis:
-            total += 1
-            pos = xi[i]
-    return total
-
-
-xi.sort()
-start = 1
-end = xi[n - 1] - xi[0]
-
-
-while start <= end:
-    gap = (start + end) // 2
-    # 개수가 충분히 안나왔을 때 -> 간격을 줄인다
-    if router_count(gap) < c:
-        end = gap - 1
+while low + 1 < high:
+    idx = 0
+    cur = 0
+    count = 1
+    mid = (low + high) // 2
+    while idx < n:
+        if house[idx] - house[cur] >= mid:
+            count += 1
+            cur = idx
+        idx += 1
+    if count < c:
+        high = mid
     else:
-        answer = gap
-        start = gap + 1
+        low = mid
 
-print(answer)
+print(low)
